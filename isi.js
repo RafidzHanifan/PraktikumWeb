9d4f711d4b95b5ad46a487b5fa427be82fab3858
RafidzHanifan <rafidz.datu007@gmail.com>
2024-10-17 20:37:13 +0700

    Delete isi.js

--- isi.js
+++
@@ -1,7 +0,0 @@
-document.writeln("<center>");
-document.writeln("<hr width=600 color=black>");
-document.writeln("<h1>UNIVERSITAS LAMPUNG</H1>");
-document.writeln("<h2>PENDIDIKAN TEKNOLOGI INFORMASI<H2>");
-document.writeln("<h3>JURUSAN PENDIDIKΑΝ ΜΑΤΕΜΑΤΙΚA DAN ILMU PENGETAHUAN ALAM<H3>");
-document.writeln("<hr width=600 color=Black>");
-document.writeln("</center>");
